41cc7a75de3749262fd57bf31f3460b284469faf
Caleb Land <caleb@land.fm>
2017-05-07 15:49:22 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# meat-and-potatoes
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'meat-and-potatoes'
+maintainer 'The Authors'
+maintainer_email 'caleb@land.fm'
+license 'MIT'
+description 'Installs/Configures meat-and-potatoes'
+long_description 'Installs/Configures meat-and-potatoes'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/meat-and-potatoes/issues'
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/meat-and-potatoes'
